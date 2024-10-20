ed65cdc79404236da91d084fc853a0cb253f5f4b
tamerkat <mahad10taha@gmail.com>
2024-10-20 13:38:16 +0300

    add admin folder

---
+++ admin/src/Components/Listproduct/Listproduct.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Listproduct.css'
+
+const Listproduct = () => {
+  return (
+    <div className='listproduct'>
+      
+    </div>
+  )
+}
+
+export default Listproduct
